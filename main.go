59cbd619652e1e3ac4da5a43d24fe32d55d6ea03
Furkan Samaraz <92402372+FurkanSamaraz@users.noreply.github.com>
2022-06-13 18:14:05 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/web3-storage/go-w3s-client"
+)
+
+func main() {
+
+	// Tokenimizi ekliyoruz.
+	conn, err := w3s.NewClient(w3s.WithToken("TOKEN"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	//Boş bir durum döndürüyoruz.
+	ctx := context.Background()
+
+	// Seçtiğimiz resim mi açıyoruz.
+	img, _ := os.Open("./ornek.png")
+
+	// Tokenimize durumu atıyoruz ve içini resimimizi ekliyoruz.
+	cid, _ := conn.Put(ctx, img)
+
+	// Url erişim çıktımızı alıyoruz ve tarayıcıya yapıştırarak erişim sağlıyoruz.
+	fmt.Printf("https://ipfs.io/ipfs/%s\n", cid)
+}
